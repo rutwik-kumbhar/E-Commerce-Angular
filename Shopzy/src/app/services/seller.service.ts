944f88d5e9ba068428447c99e708ee74f13a35b8
Rutwik Kumbhar <kumbharrutwik@gmail.com>
2023-10-17 23:46:11 +0530

    Auth-Guard Added for seller

--- Shopzy/src/app/services/seller.service.ts
+++ Shopzy/src/app/services/seller.service.ts
@@ -1,17 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { SignUp } from '../data-type';
+import {Router} from '@angular/router'
 
 @Injectable({
   providedIn: 'root',
 })
 export class SellerService {
+
+  isSellerLoggedIn = new BehaviorSubject<boolean>(false);
   private baseUrl: string = 'http://localhost:3000/';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,private router:Router) {}
 
-  sellerSignUp(data: SignUp): Observable<Object>{
-    return this.http.post(`${this.baseUrl}`+"seller", data);
+  sellerSignUp(data: SignUp){
+   this.http.post(`${this.baseUrl}`+"seller", data,{observe:'response'}).subscribe((result)=>{
+        this.isSellerLoggedIn.next(true);
+        localStorage.setItem('seller',JSON.stringify(result.body))
+        this.router.navigate(['seller-home'])
+    });
+  }
+
+  relodeSeller(){
+    if(localStorage.getItem('seller')){
+      this.isSellerLoggedIn.next(true);
+      this.router.navigate(['seller-home'])
+    }
   }
 }
